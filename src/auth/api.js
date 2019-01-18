0785f9d3c86cdc500fd13abaea0ce6d8f86381d8
naturalbornchiller <mail@emmanuelprice.com>
2019-01-18 10:12:46 -0500

    Removes slash from url AGAIN.

--- src/auth/api.js
+++ src/auth/api.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://murmuring-lowlands-80865.herokuapp.com/'
+const apiUrl = 'https://murmuring-lowlands-80865.herokuapp.com'
 
 export const handleErrors = res => {
   if (res.ok) {
