3bff6fe903102653f9faa006f0170879a7c0d306
naturalbornchiller <mail@emmanuelprice.com>
2019-01-18 09:52:04 -0500

    Changes urls to production

--- src/auth/api.js
+++ src/auth/api.js
@@ -1,4 +1,4 @@
-const apiUrl = 'http://localhost:4741'
+const apiUrl = 'https://murmuring-lowlands-80865.herokuapp.com/'
 
 export const handleErrors = res => {
   if (res.ok) {
--- src/tasks/api.js
+++ src/tasks/api.js
@@ -1,4 +1,4 @@
-const apiUrl = 'http://localhost:4741'
+const apiUrl = 'https://murmuring-lowlands-80865.herokuapp.com/'
 import axios from 'axios'
 
 export const taskIndex = user => (
