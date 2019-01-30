5d8980b623a50fc8e7e3e1c5aa065bc7e7016f02
naturalbornchiller <mail@emmanuelprice.com>
2019-01-30 10:15:16 -0500

    Switches to materialui tabs
    Now if there are too many tabs, it overflows
    cleanly.

--- src/tasks/components/TaskIndex.js
+++ src/tasks/components/TaskIndex.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
-import { Tabs, Tab } from 'react-bootstrap'
+// import { Tabs, Tab } from 'react-bootstrap'
+import { Tabs, Tab, Typography } from '@material-ui/core'
 import Task from './Task'
 import TaskPost from './TaskPost'
 import { Link, Route, withRouter } from 'react-router-dom'
 import { taskIndex } from '../api'
 import '../Task.scss'
 
-export default class TaskIndex extends Component {
+
+const tabStyle = {
+  textDecoration: 'none',
+  fontSize: '15px',
+  borderTop: '1px solid rgba(0,0,0,.2)',
+  borderBottom: '1px solid rgba(0,0,0,.2)'
+}
+
+class TaskIndex extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      tasks: []
+      tasks: [],
+      value: null
     }
   }
 
@@ -25,24 +35,48 @@ export default class TaskIndex extends Component {
       .catch(console.error)
   }
 
+  handleChange = (event, value) => {
+    this.setState({ value })
+  }
+
+  handleChangeIndex = index => {
+    this.setState({ value: index })
+  }
+
   render() {
     return (
       <React.Fragment>
         <div>
           <h2 className="task-banner">Tasks</h2>
           <TaskPost user={ this.props.user }  taskIndex={ this.taskIndex } />
-          <Tabs>
+          <Tabs
+            value={this.state.value}
+            onChange={this.handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="scrollable"
+            scrollButtons="auto"
+          >
             { this.state.tasks.map((task, i) => (
-              <Tab key={ i } eventKey={ task.name } title={ task.name } >
-                <Task user={ this.props.user }
-                  id={ task._id }
-                  data={ task }
-                  taskIndex={ this.taskIndex } />
-              </Tab>
+              <Tab
+                key={ i }
+                label={ task.name }
+                component={ Link }
+                to={ `/tasks/${task._id}` }
+                style={ tabStyle } />
             ))}
           </Tabs>
+        </div>
+        <div className="selected-task-container">
+          <Route path='/tasks/:id' render={ ({ match }) => (
+            <Task user={ this.props.user }
+              id={ match.params.id } 
+              taskIndex={ this.taskIndex } />
+          )} />
         </div>
       </React.Fragment>
     )
   }
 }
+
+export default TaskIndex
