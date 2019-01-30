983bd10a5ecb1cacb757c6c93a2ec501c4e6042c
naturalbornchiller <mail@emmanuelprice.com>
2019-01-30 18:55:44 -0500

    Still need to fix up responsivity of about/ page
    About to implement the calendar

--- src/home/Home.js
+++ src/home/Home.js
@@ -3,7 +3,7 @@ import './Home.scss'
 const Home = () => (
   <div>
     <div className="about-the-app">
-      <h1>About The App</h1>
+      <h1 className="about-header">About The App</h1>
       <p><strong>Concatenate</strong> is an app designed to display progress. The concept is simple. Users create tasks to track. After completing the task, and adding to the chain (a visual representation of the number of days in a row a task has been completed), a user is able to view the fruits of repitition and incremental growth. In this way, an incentive is implicitly formed:</p>
       <p>Don&lsquo;t break the chain!</p>
     </div>
