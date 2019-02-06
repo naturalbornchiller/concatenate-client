88708b9493c62922c749a07aaf2e0d76e49e9ebe
naturalbornchiller <mail@emmanuelprice.com>
2019-02-06 17:29:24 -0500

    Calendar works. Chains are now visible.

--- src/tasks/components/Task2.js
+++ src/tasks/components/Task2.js
@@ -1,29 +1,37 @@
 import React, { Component } from 'react'
 import { taskShow, taskDelete, taskPatch } from '../api'
 import { Calendar, CalendarControls } from 'react-yearly-calendar'
-import moment from 'moment'
+import Moment from 'moment'
+import { extendMoment } from 'moment-range'
+const moment = extendMoment(Moment)
 import '../Calendar.scss'
 import '../Task.scss'
 
 class Task2 extends Component {
-
+  
   constructor(props) {
     super(props)
     const today = moment()
+
     this.state = {
       year: today.year(),
       task: {
         name: '',
         chains: []
+      },
+      customClasses: {
+        'range-left': [],
+        'range': [],
+        'range-right': []
       }
     }
   }
 
+  /* Component Event Handlers */
   componentDidMount () {
     // make request to the api for selected task
     this.taskShow()
   }
-
   componentDidUpdate (prevProps) {
     // hen a different link is clicked
     if (this.props.id !== prevProps.id) {
@@ -31,39 +39,65 @@ class Task2 extends Component {
     }
   }
 
+  /* API */
   taskShow = () => (
     taskShow(this.props)
       .then(({ data }) => this.setState({ task: data.task }))
+      .then(this.convertChainToRangeOfDays)
       .catch(console.error)
   )
-
   taskDelete = () => (
     taskDelete(this.props)
       .then(this.props.taskIndex)
       .catch(console.error)
   )
-
   taskPatch = () => (
     taskPatch(this.props)
       .then(() => this.taskShow())
       .catch(console.error)
   )
 
-  customClasses = day => (
-    this.state.task.chains.forEach(chain => {
-      if (day.isSame(chain.dayStarted, 'day')) return 'range-left'
-      if (day.isSame(chain.dayBroken, 'day')) return 'range-right'
-      if (day.isBetween(chain.dayStarted, chain.dayBroken || chain.lastConcat)) {
-        return 'range'
+  /* Helpers */
+  convertChainToRangeOfDays = () => {
+    // store dates for which css will be applied
+    const rangeStart = [], rangeEnd = [], range = []
+
+    // push significant dates to arrays
+    this.state.task.chains.forEach(({ dateStarted, dateBroken, lastConcat }) => {
+      // store last day in chain
+      const lastDay = dateBroken || lastConcat
+
+      // create enumerable range from start to end date
+      const rng = moment.range(moment(dateStarted), moment(lastDay))
+
+      // add each date to the rangeClass array
+      for (const day of rng.by('day')) {
+        range.push(day.format('YYYY-MM-DD'))
       }
+
+      // add first and last days to respective arrays
+      rangeEnd.push(moment(lastDay).format('YYYY-MM-DD'))
+      rangeStart.push(moment(dateStarted).format('YYYY-MM-DD'))
     })
 
-  )
+    // format class object
+    const classes = {
+      range,
+      'range-left': rangeStart, 
+      'range-right': rangeEnd
+    }
 
+    // set state with custom Calendar classes
+    this.setState({ customClasses: classes })
+  }
+
+  /* Content */
   render() {
     const {
+      customClasses,
       year
     } = this.state
+    console.log(customClasses)
     return (
       <div id="calendar">
         <CalendarControls
@@ -72,7 +106,7 @@ class Task2 extends Component {
         <Calendar
           year={year}
           showWeekSeparators={false}
-          customClasses={this.customClasses}
+          customClasses={customClasses}
         />
       </div>
     )
